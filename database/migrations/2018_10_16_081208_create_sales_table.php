ca580e657b1d8f49776a6f753a6b787fba8d2b15
BryanTsy97 <btsy0407@gmail.com>
2018-11-21 21:13:17 +0800

    Added foreign key relationship for database

--- database/migrations/2018_10_16_081208_create_sales_table.php
+++ database/migrations/2018_10_16_081208_create_sales_table.php
@@ -14,7 +14,7 @@ class CreateSalesTable extends Migration
     public function up()
     {
         Schema::create('sales', function (Blueprint $table) {
-            
+
             $table->increments('id');
             $table->string('itemID');
             $table->integer('itemQty');
